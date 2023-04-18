3dc42b0b3f5607fa3cb9e738779b2387e89af33a
Mandisa2526 <92437885+Mandisa2526@users.noreply.github.com>
2023-04-18 14:01:08 +0200

    Update text-bill.js

--- js/text-bill.js
+++ js/text-bill.js
@@ -10,3 +10,8 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+
+
+var textBox = document.querySelector(".billTypeText");
+var addBtn = document.querySelector(".addToBillBtn");
+var 
