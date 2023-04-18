89b9577be363e54802e510514b82d69deccad26d
Mandisa2526 <92437885+Mandisa2526@users.noreply.github.com>
2023-04-18 15:21:21 +0200

    Update text-bill.js

--- js/text-bill.js
+++ js/text-bill.js
@@ -14,4 +14,40 @@
 
 var textBox = document.querySelector(".billTypeText");
 var addBtn = document.querySelector(".addToBillBtn");
-var 
+
+var callsTotal = 0;
+var smsTotal = 0;
+
+function textBillTotal(){
+    // get the value entered in the billType textfield
+    var billTypeEntered = billTypeText.value.trim();
+    // update the correct total
+    if (billTypeEntered === "bill"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    
+    //update the totals that is displayed on the screen.
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+     //update the totals that is displayed on the screen.
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+    
+    //color the total based on the criteria
+    if (totalCost >= 50){
+        // adding the danger class will make the text red
+        totalCostElem.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalCostElem.classList.add("warning");
+    }
+}
+
+textTotalAddBtn.addEventListener('click', textBillTotal);
